3886567b47d1106c4b2de91a924647d3312f557e
k452b <sajibbaruanstu@gmail.com>
2024-06-30 11:09:23 +0600

    init

---
+++ src/test/java/net/javaguides/Employee_Management_System/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.Employee_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
